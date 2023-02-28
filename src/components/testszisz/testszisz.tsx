3ac11df95c013b033f7cc47091afbff5f13883d5
Szissz007 <szaszsziszi20030210@gmail.com>
2023-02-28 22:18:27 +0100

    nekemkicsi

--- src/components/testszisz/testszisz.tsx
+++ src/components/testszisz/testszisz.tsx
@@ -6,7 +6,11 @@ export class TestSzisz extends React.Component<{}, {}> {
 render(){
     return <div className="contactContainer" style={{fontSize: '28px'}}>        
                 <div className="contactTitle">Sziszke the biggest cock in the world:</div>     
-                <div className="valamiSzovegClass">valami szöveg</div>                
+                <div className="valamiSzovegClass">valami szöveg</div>  
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>           
            </div>                
     }
 }
