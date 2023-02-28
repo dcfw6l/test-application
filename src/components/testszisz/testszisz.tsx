fce514e8acb4b03354976ab3c7105ceaf7a12da4
szkristof <szaszkristooon99@gmail.com>
2023-02-28 22:08:26 +0100

    valami szöveg

--- src/components/testszisz/testszisz.tsx
+++ src/components/testszisz/testszisz.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import "./testszisz.css"
+import "./testszisz.css";
 
 export class TestSzisz extends React.Component<{}, {}> {
 
 render(){
     return <div className="contactContainer" style={{fontSize: '28px'}}>        
-        <div className="contactTitle">Sziszke the biggest cock in the world:</div>         
-        </div>                
+                <div className="contactTitle">Sziszke the biggest cock in the world:</div>     
+                <div className="valamiSzovegClass">valami szöveg</div>                
+           </div>                
     }
 }
