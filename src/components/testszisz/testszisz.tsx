e128811f180f1f82dcaea90f8060bd8ceefac7ca
Szissz007 <szaszsziszi20030210@gmail.com>
2023-03-02 22:34:55 +0100

    egy_kis_update_xd

--- src/components/testszisz/testszisz.tsx
+++ src/components/testszisz/testszisz.tsx
@@ -4,9 +4,39 @@ import "./testszisz.css";
 export class TestSzisz extends React.Component<{}, {}> {
 
 render(){
-    return <div className="contactContainer" style={{fontSize: '28px'}}>        
-                <div className="contactTitle">Sziszke the biggest cock in the world:</div>     
-                <div className="valamiSzovegClass">valami szöveg</div>  
+    return <div className="headerContainersajat">        
+                <div className="titleTextsajat">Szász Szilárd</div>     
+                <div className="valamiSzovegClass"><b>valami szöveg</b></div>  
+                <img src="szilard.jpg" alt="Legnagyobb sztár"></img>
+            <div className="contactContainerForTable_sajat">
+            <table style={{width: '80%'}}>        
+            <thead>
+                <tr>
+                    <th>Kor:</th>
+                    <th>Testtömeg:</th>
+                    <th>Fekvenyomás:</th>
+                    <th>Guggolás:</th>
+                    <th>Felhúzás:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>19</td>
+                    <td>89 kg</td>
+                    <td>110 kg</td>
+                    <td>150 kg</td>
+                    <td>180 kg</td>
+                </tr>
+                <tr>
+                    <td>21</td>
+                    <td>95 kg</td>
+                    <td>130 kg</td>
+                    <td>180 kg</td>
+                    <td>210 kg</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
                 <div>
                     <div></div>
                     <div></div>
